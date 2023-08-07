e15cd55bcaaab7c7152c7c72a861f2f4be958def
stachurskijeff <134335054+stachurskijeff@users.noreply.github.com>
2023-08-07 17:34:55 +0200

    third commit

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-const _ = require('lodash');
+const { readFile, writeFile } = require('fs')
 
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems)
-console.log('hello people')
+console.log('started a first task')
+// CHECK FILE PATH!!!!
+readFile('./content/first.txt', 'utf8', (err, result) => {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log(result)
+  console.log('completed first task')
+})
+console.log('starting next task')
