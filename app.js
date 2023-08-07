6c3d082b6bedb8ae6af272aae65ac023dd19a47a
stachurskijeff <134335054+stachurskijeff@users.noreply.github.com>
2023-08-07 19:50:03 +0200

    fourth commit

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-const { readFile, writeFile } = require('fs')
+const { readFile } = require("fs");
+const path = require("path");
 
-console.log('started a first task')
-// CHECK FILE PATH!!!!
-readFile('./content/first.txt', 'utf8', (err, result) => {
-  if (err) {
-    console.log(err)
-    return
+const getText = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+// getText("./content/first.txt")
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
+
+const start = async () => {
+  try {
+    const first = await getText("./content/first.txt");
+    const second = await getText("./content/second.txt");
+
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
   }
-  console.log(result)
-  console.log('completed first task')
-})
-console.log('starting next task')
+};
+
+start();
