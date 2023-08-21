ff85f6244852abe404d93da352a50981e6bca1b8
stachurskijeff <134335054+stachurskijeff@users.noreply.github.com>
2023-08-21 18:48:12 +0200

    seventh commit

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,9 @@
-console.log('Express Tutorial')
+const http = require("http")
+
+const server = http.createServer((req, res)=>{
+    console.log('user hit the sever')
+    res.end('home page')
+})
+
+server.listen(2137)
+
