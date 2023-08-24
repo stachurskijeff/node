e4d7fd139c645d18862b382bc87cbb3102fe15f9
stachurskijeff <134335054+stachurskijeff@users.noreply.github.com>
2023-08-24 17:35:21 +0200

    ten commit

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,9 +1,52 @@
-const http = require("http")
+const http = require("http");
+const {readFileSync} = require('fs');
 
-const server = http.createServer((req, res)=>{
-    console.log('user hit the sever')
-    res.end('home page')
-})
+// get all files
+const homePage = readFileSync('./navbar-app/index.html')
+const homeStyles = readFileSync('./navbar-app/styles.css')
+const homeImage = readFileSync('./navbar-app/logo.svg')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
 
-server.listen(2137)
+const server = http.createServer((req, res) => {
+    // console.log(req.method)
+    const url = req.url;
+    if(url === '/'){
+        console.log(url)
+    //home page
+    res.writeHead(200, { 'content-type':'text/html' })
+res.write(homePage)
+    res.end()
+    }
+    // about page
+    else if(url === '/about'){
+        res.writeHead(200, { 'content-type':'text/html' })
+        res.write("<h1>about page</h1>")
+            res.end()
+    }
+    // styles
+    else if(url === '/styles.css'){
+        res.writeHead(200, { 'content-type':'text/css' })
+        res.write(homeStyles)
+            res.end()
+    }
+    // image/logo
+    else if(url === '/logo.svg'){
+        res.writeHead(200, { 'content-type':'image/svg+xml' })
+        res.write(homeImage)
+            res.end()
+    }
+    // logic
+    else if(url === '/browser-app.js'){
+        res.writeHead(200, { 'content-type':'text/javascript' })
+        res.write(homeLogic)
+            res.end()
+    }
+    // 404
+    else {
+        res.writeHead(404, { 'content-type':'text/html' })
+        res.write("<h1>page not found</h1>")
+            res.end()
+    }
+});
 
+server.listen(2137);
